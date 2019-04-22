7d3aee6ee2b718c9b64e7591904be3133081536b
datavelizar <datavelizar@gmail.com>
2019-04-22 21:41:05 +0300

    Started Page Object Model refactoring

--- PomOfTestProgramForSeleniumWebDriver/Core/Driver.cs
+++ PomOfTestProgramForSeleniumWebDriver/Core/Driver.cs
@@ -6,14 +6,26 @@ using System.Text;
 
 namespace PomOfTestProgramForSeleniumWebDriver.Core
 {
-   public class Driver
+    public static class Driver
     {
+        public static IWebDriver Initialize()
+        {
+            var driver = new ChromeDriver(Environment.CurrentDirectory);//new FirefoxDriver(); // new PhantomJSDriver();//
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
 
-        IWebDriver driver = new ChromeDriver(); //new FirefoxDriver(); // new PhantomJSDriver();//
+            return driver;
+        }
 
-
-        //driver.Manage().Window.Maximize();
-        //                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-
+       
     }
 }
+
+//IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));//working
+//IWebDriver driver = new ChromeDriver(Environment.CurrentDirectory);//working without reflection libraries
+//IWebDriver driver = new ChromeDriver(); //new FirefoxDriver(); // new PhantomJSDriver();//
+//driver.Manage().Window.Maximize();
+//driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+////Whole Category "IT - Sowtware"; Keyword is String.Empty, Category is 15
+//driver.Url = "https://www.jobs.bg/front_job_search.php?frompage=";
--- PomOfTestProgramForSeleniumWebDriver/Program.cs
+++ PomOfTestProgramForSeleniumWebDriver/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PomOfTestProgramForSeleniumWebDriver.Core;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace PomOfTestProgramForSeleniumWebDriver
 {
@@ -6,6 +11,11 @@ namespace PomOfTestProgramForSeleniumWebDriver
     {
         static void Main(string[] args)
         {
+            var driver = Driver.Initialize();
+            driver.Url = "https://www.jobs.bg/front_job_search.php?frompage=";
+            
+            //aaa.browser.Url = "https://www.jobs.bg/front_job_search.php?frompage="; //ComposeURLInJobsbg("", 15, 0);
+
             Console.WriteLine("Hello World!");
         }
     }
